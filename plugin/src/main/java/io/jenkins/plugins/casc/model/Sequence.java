ff35ec0952d1eb63a03fc1b35663194b48301f09
Joseph Petersen <josephp90@gmail.com>
2019-03-23 21:37:57 +0100

    add equals and hash override for Sequence
    Pleases SpotBugs

--- plugin/src/main/java/io/jenkins/plugins/casc/model/Sequence.java
+++ plugin/src/main/java/io/jenkins/plugins/casc/model/Sequence.java
@@ -1,6 +1,7 @@
 package io.jenkins.plugins.casc.model;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 /**
  * @author <a href="mailto:nicolas.deloof@gmail.com">Nicolas De Loof</a>
@@ -42,5 +43,25 @@ public final class Sequence extends ArrayList<CNode> implements CNode {
         final Sequence clone = new Sequence();
         stream().map(CNode::clone).forEach(clone::add);
         return clone;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        if (!super.equals(o)) {
+            return false;
+        }
+        Sequence sequence = (Sequence) o;
+        return Objects.equals(source, sequence.source);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), source);
     }
 }
